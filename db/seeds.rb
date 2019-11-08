fe2fb679f02960ba1b48d95b116bd266d487cb16
ichihara0621 <ichihara.misato@lmi.ne.jp>
2019-11-08 10:29:08 +0000

    seeddb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             address: "tokyo_suginami",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+50000.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  address = Faker::Address.city
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               address: address,
+               password:              password,
+               password_confirmation: password)
+end
